debdd3ed8b328a0c69a49b141941e022ab9f87c6
skreynolds <shane.k.reynolds@gmail.com>
2020-04-25 11:12:56 +0930

    Update: completed agent implementation

--- 01_cross_entropy/agent.py
+++ 01_cross_entropy/agent.py
@@ -1,15 +1,10 @@
 # import requireed libraries
-import gym
 import math
-import numpy as np
-from collections import deque
-import matplotlib,pyplot as pyplot
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent(nn.Module):
 	def __init__(self, env, h_size=16):
@@ -34,4 +29,38 @@ class Agent(nn.Module):
 		x = F.tanh(self.fc2(x))
 		return x.cpu().data
 
-	
+	def set_weights(self, weights):
+		
+		# initialise weights matrices sizes
+		s_size = self.s_size
+		h_size = self.h_size
+		a_size = self.a_size
+
+		# separate the weights for each layer
+		fc1_end = (s_size*h_size) + h_size
+		fc1_W = torch.from_numpy(weights[:s_size*h_size].reshape(s_size, h_size))
+		fc1_b = torch.from_numpy(weights[s_size*h_size:fc1_end])
+		fc2_W = torch.from_numpy(weights[fc1_end:fc1_end + (h_size*a_size)].reshape(h_size, a_size))
+		fc2_b = torch.from_numpy(weights[fc1_end+(h_size*a_size):])
+
+		# set the weights for each layer
+		self.fc1.weight.data.copy_(fc1_W.view_as(self.fc1.weight.data))
+		self.fc1.bias.data.copy_(fc1_b.view_as(self.fc1.bias.data))
+		self.fc2.weight.data.copy_(fc2_W.view_as(self.fc2.weight.data))
+		self.fc2.bias.data.copy_(fc2_b.view_as(self.fc2.bias.data))
+
+	def get_weights_dim(self):
+		return (self.s_size+1)*self.h_size + (self.h_size+1)*self.a_size
+
+	def evaluate(self, weights, gamma=1.0, max_t=5000):
+		self.set_weights(weights)
+		episode_return = 0.0
+		state = self.env.reset() # resets the environment
+		for t in range(max_t):
+			state = torch.from_numpy(state).float().to(device)
+			action = self.forward(state)
+			state, reward, done, _ = self.env.step(action)
+			episode_return += reward * math.pow(gamma, t)
+			if done:
+				break
+		return episode_return
